591ccea1a1dfba7052b25d61e10fd0d724f6839c
taye000 <taylorgitari@gmail.com>
2021-11-24 17:27:45 +0300

    project update

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,65 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  //Overview page
+  if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, { "content-type": "text/html" });
+
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el));
+    res.end(tempOverview);
+
+    //Product page
+  } else if (pathName === "/product") {
+    res.end("This is the product");
+
+    //API
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(data);
+
+    //Not found
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+    });
+    res.end("<h1>page not found</h1>");
+  }
+});
+
+server.listen(3000, "127.0.0.1", () => {
+  console.log("listening on port 3000");
+});
